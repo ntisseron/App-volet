0d50dd157a9a31fd79462bff07dbde5104169ebf
Nathan <153722520+ntisseron@users.noreply.github.com>
2025-01-27 18:38:09 +0100

    nv 2

---
+++ Acceuil/script.js
@@ -0,0 +1,31 @@
+function redirectToNathan() {
+    window.location.href = '../Chambre/Nathan.html';
+}
+
+function redirectToLisa() {
+    window.location.href = '../Chambre/Lisa.html';
+}
+
+function redirectToParents() {
+    window.location.href = '../Chambre/Parents.html';
+}
+
+function redirectToAmis() {
+    window.location.href = '../Chambre/Amis.html';
+}
+
+function redirectToBureau() {
+    window.location.href = '../Chambre/Bureau.html';
+}
+
+function redirectToVolets() {
+    window.location.href = '../Chambre/Volets.html';
+}
+
+function redirectToPiscine() {
+    window.location.href = '../Chambre/Piscine.html';
+}
+
+function redirectToAutres() {
+    window.location.href = '../Chambre/Autres.html';
+}
