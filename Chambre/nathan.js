f468b005ff1cf8fd92bff0a135eecc1dc080c9bb
Nathan <153722520+ntisseron@users.noreply.github.com>
2025-01-29 18:00:24 +0100

    api et cors

--- Chambre/nathan.js
+++ Chambre/nathan.js
@@ -10,12 +10,17 @@ function toggleMode() {
         contentWrapper.classList.remove('night-mode');
         voletImage.src = '../assets/Volets.png';
     }
+
+    // Envoyer une requête API
+    sendApiRequest(isChecked);
 }
 
 function toggleNewContentWrapper() {
     const newContentWrapper = document.getElementById('newContentWrapper');
     const newVoletImage = document.getElementById('newVoletImage');
-    if (document.querySelector('.cb').checked) {
+    const isChecked = document.querySelector('.cb').checked;
+
+    if (isChecked) {
         newContentWrapper.classList.add('yellow-background');
         newContentWrapper.classList.remove('black-background');
         newVoletImage.src = '../assets/ampoule.png';
@@ -24,4 +29,30 @@ function toggleNewContentWrapper() {
         newContentWrapper.classList.remove('yellow-background');
         newVoletImage.src = '../assets/ampouleEteint.png';
     }
+
+    // Envoyer une requête API
+    sendApiRequest(isChecked);
+}
+
+function sendApiRequest(isChecked) {
+    const urlOn = 'http://192.168.0.49/core/api/jeeApi.php?apikey=WPsynhMvFe6gLnEkWmqXcM3Jv66g84lm&type=cmd&id=490'; // Remplacez par l'URL de votre API pour allumer
+    const urlOff = 'http://192.168.0.49/core/api/jeeApi.php?apikey=WPsynhMvFe6gLnEkWmqXcM3Jv66g84lm&type=cmd&id=491'; // Remplacez par l'URL de votre API pour éteindre
+
+    const url = isChecked ? urlOn : urlOff;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        mode: 'cors', // Ajoutez cette ligne pour les requêtes CORS
+        body: JSON.stringify({ state: isChecked ? 'on' : 'off' })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
